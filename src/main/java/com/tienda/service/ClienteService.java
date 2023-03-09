d4c03cce67dc75c21f0759df5dc7c42c43cbaa82
Clitfor <Clitfor@DESKTOP-MDR2OFU>
2023-03-09 04:30:07 +0100

    Reconstruccion de proyecto

---
+++ src/main/java/com/tienda/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.tienda.service;
+
+import com.tienda.domain.Cliente;
+import java.util.List;
+
+public interface ClienteService {
+
+    public List<Cliente> getClientes();
+    
+    public Cliente getCliente(Cliente cliente);
+    
+    public void save(Cliente cliente); // para insertar o modificar(Si viene el idCliente o no)
+    
+    public void delete(Cliente cliente);
+    
+}
