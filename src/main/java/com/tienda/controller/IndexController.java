7d82fe24099f9065a5f36429c893453cf4acc9b0
Clitfor <Clitfor@DESKTOP-MDR2OFU>
2023-03-14 02:53:51 +0100

    Semana 8 y tarea

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,6 +1,6 @@
 package com.tienda.controller;
 
-import com.tienda.service.ClienteService;
+import com.tienda.service.ArticuloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,14 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @Autowired
-    ClienteService clienteService;
+    ArticuloService articuloService;
 
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Ahora desde MVC");
 
-        var clientes = clienteService.getClientes();
-        model.addAttribute("clientes", clientes);
+        var articulos = articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
 
         return "index";
     }
